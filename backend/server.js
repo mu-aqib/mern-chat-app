1af2e905989c5188727c4c18e73c88c26d3ab754
Mac <aqibafridi0345@gmail.com>
2022-10-04 16:15:57 +0500

    routes with chats data

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,23 @@
 const express = require('express'); 
+const chats  = require('./data/data')
+
 const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, console.log("now server has been started"))
+
+
+app.get('/', (req, res)=>{
+    res.send("Welcome to home page")
+} )
+
+app.get('/chats', (req, res)=>{
+    res.send(chats)
+} ) 
+
+app.get('/chats/:id', (req, res)=>{
+    const id = req.params.id;
+    let chat = chats.find(chat=>{
+        return chat._id === id;
+    })
+    res.send(chat)
+} ) 
+
+app.listen(5000, console.log("now server has been started")) 
