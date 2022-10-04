01ee30b9c76ebd0958848bccd8ab5497fa36f768
Mac <aqibafridi0345@gmail.com>
2022-10-04 16:19:49 +0500

    some configuration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
+const dotenv = require('dotenv');
 const chats  = require('./data/data')
 
 const app = express();
-
+dotenv.config();
 
 app.get('/', (req, res)=>{
     res.send("Welcome to home page")
@@ -20,4 +21,5 @@ app.get('/chats/:id', (req, res)=>{
     res.send(chat)
 } ) 
 
-app.listen(5000, console.log("now server has been started")) 
+const PORT = process.env.PORT || 5000;
+app.listen(5000, console.log("now server has been started on port " + PORT)) 
