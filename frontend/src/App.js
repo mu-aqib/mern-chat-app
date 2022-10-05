0f055b9b0b2d74fcf32d32979006332cc03e979e
Mac <aqibafridi0345@gmail.com>
2022-10-05 16:01:52 +0500

    auth files with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./App.css";
-import Nav from "./components/nav/Nav";
+// import Nav from "./components/nav/Nav";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// defiened components
 import ChatBody from "./components/chatBody/ChatBody";
+import Authentication from "./components/Authentication/login";
 
 function App() {
   return (
-    <div className="__main">
-      {/* <Nav /> */}
-      <ChatBody />
-    </div>
+      <div className="__main">
+        <Router>
+          <Routes>
+              <Route path="/" element={ <Authentication /> } />
+              <Route path="/chat" element={ <ChatBody /> } />
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
