a9cc117f8d98574017d7d4b8c63c4e46adad6e5f
Mac <aqibafridi0345@gmail.com>
2022-10-07 16:01:33 +0500

    expiry date changed

--- backend/config/generatToken.js
+++ backend/config/generatToken.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+// const expiry_date = 1*24*60*60; // 1day
 let generateToken = (id)=>{
-    const secret_key = process.env.JWT_SECRET;
-    // return jwt.sign(id.toJSON(), process.env.JWT_SECRET, {
-    //     expiresIn: '60d',
-    // })
-    // jwt.sign( id.toJSON(), process.env.JWT_SECRET, { expiresIn: 60 * 60});
-    return jwt.sign({id}, secret_key, {
-        expiresIn: 140000000003
+    // const secret_key = process.env.JWT_SECRET;
+    
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '2d'
     });
 }
 
