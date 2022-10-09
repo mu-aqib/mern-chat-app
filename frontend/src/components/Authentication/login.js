19b27b6bf86f215a92caa06e06ebc8612d0ffb04
Mac <aqibafridi0345@gmail.com>
2022-10-10 02:12:44 +0500

    programmatic navigation when logged in

--- frontend/src/components/Authentication/login.js
+++ frontend/src/components/Authentication/login.js
@@ -3,41 +3,34 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
 function Login() {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  let navigate = useNavigate();
-
-  const onChangeValue = (e)=> {
-    this.setState( {
-      [e.target.name] : e.target.value
-    })
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPass] = useState('');
+  const navigate = useNavigate();
 
   const submitHandler = async (e)=> {
     e.preventDefault();
-    alert();
-    const state = this.state;
-    if(!state.email || !state.password) {
-        alert('please fill all required (*) field'); 
-        return
+    if(!email || !password) {
+      alert('please fill all required (*) field'); 
+      return
     }
 
     try{
-        const config = {
-            header: {"Content-type": "application/json",}
-        }
+      const config = {
+          header: {"Content-type": "application/json",}
+      }
 
-        let {data} = await axios.post('api/user/login', {
-                email: state.email,
-                password: state.password
-        } , config)
+      let {data} = await axios.post('api/user/login', {
+        email,
+        password
+      } , config)
 
-        if(data){
-          navigate('/chat')
-        }
+      if(data){
+        alert();
+        navigate('/chat')
+      }
     }
     catch(err){
-        console.log(err)
+      console.log(err)
     }
   }
 
@@ -46,9 +39,9 @@ function Login() {
       <div className='form-box'>
         <h2>login</h2>
         <form onSubmit={(e) => submitHandler(e)}>
-          <input name='email' type="email" placeholder="email *" onChange={(e) => onChangeValue(e)}/>
+          <input name='email' type="email" placeholder="email *" onChange={(e) => setEmail(e.target.value)}/>
         
-          <input name='password' type="password" placeholder="password *" onChange={(e) => onChangeValue(e)}/>
+          <input name='password' type="password" placeholder="password *" onChange={(e) => setPass(e.target.value)}/>
 
           <button type='submit' className="btn btn-info"> <span>login</span> </button>
         </form>
