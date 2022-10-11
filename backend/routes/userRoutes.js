c1c11ed1b95ae9608b05f47c8981dbfe316ec23f
Mac <aqibafridi0345@gmail.com>
2022-10-11 23:56:34 +0500

    request users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { userRegistration } = require('../controllers/userController')
-const { userLogin } = require('../controllers/userController')
+const { userRegistration , userLogin , getAllUsers } = require('../controllers/userController')
 
 const route = express.Router();
 
-route.route('/').post(userRegistration)
+route.route('/').post(userRegistration).get(getAllUsers)
 route.post('/login', userLogin)
 
 module.exports = route;
