78576d4fe4d905d0d8bb36c0f3ff8a7f18b53bca
Mac <aqibafridi0345@gmail.com>
2022-10-16 23:51:56 +0500

    auth frontend routes

--- frontend/src/Context/chatContext.js
+++ frontend/src/Context/chatContext.js
@@ -1,9 +1,22 @@
-import React, {useState, createContext, useContext} from 'react'
+import React, {useState, createContext, useContext, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const chat = createContext();
 
 const ChatContext = ({children}) => {
-    const [user, setUser] = useState()
+    const navigate = useNavigate()
+    const [user, setUser] = useState();
+
+    useEffect(()=>{
+        const user_info = JSON.parse(localStorage.getItem('userInfo'))
+
+        setUser(user_info)
+
+        if(!user_info){
+            navigate('/')
+        }
+    },[])
+
     return  (
         <chat.Provider value={{user, setUser}}> {children} </chat.Provider>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import chatContext from './Context/ChatContext'
+import ChatContext from './Context/ChatContext'
 
 ReactDOM.render(
-  <chatContext>
+  <ChatContext>
     <App />
-  </chatContext>,
+  </ChatContext>,
   document.getElementById('root')
 );
 
