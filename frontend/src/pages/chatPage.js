821ff9d798a26a582030c6ff9356603edab80579
Mac <aqibafridi0345@gmail.com>
2022-10-16 23:57:22 +0500

    pages

---
+++ frontend/src/pages/chatPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import '../';
+
+export default class ChatBody extends Component {
+  render() {
+    return (
+      <div className="main__chatbody">
+        <ChatBody />
+      </div>
+    );
+  }
+}
