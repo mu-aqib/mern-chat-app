70c130c3e6079b0d8306b440487e7e9c6142d80f
Mac <aqibafridi0345@gmail.com>
2022-11-02 00:14:52 +0500

    chat body selection on condition wise

--- frontend/src/components/chatContent/ChatContent.js
+++ frontend/src/components/chatContent/ChatContent.js
@@ -1,163 +1,12 @@
-// import React, { Component, useState, createRef, useEffect } from "react";
-
+import React, { createRef, useEffect, useState } from 'react'
 import "./chatContent.css";
 import Avatar from "../chatList/Avatar";
 import ChatItem from "./ChatItem";
-
-// export default class ChatContent extends Component {
-//   messagesEndRef = createRef(null);
-  // chatItms = [
-  //   {
-  //     key: 1,
-  //     image:
-  //       "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
-  //     type: "",
-  //     msg: "Hi Tim, How are you?",
-  //   },
-  //   {
-  //     key: 2,
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-  //     type: "other",
-  //     msg: "I am fine.",
-  //   },
-  //   {
-  //     key: 3,
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-  //     type: "other",
-  //     msg: "What about you?",
-  //   },
-  //   {
-  //     key: 4,
-  //     image:
-  //       "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
-  //     type: "",
-  //     msg: "Awesome these days.",
-  //   },
-  //   {
-  //     key: 5,
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-  //     type: "other",
-  //     msg: "Finally. What's the plan?",
-  //   },
-  //   {
-  //     key: 6,
-  //     image:
-  //       "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
-  //     type: "",
-  //     msg: "what plan mate?",
-  //   },
-  //   {
-  //     key: 7,
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
-  //     type: "other",
-  //     msg: "I'm taliking about the tutorial",
-  //   },
-  // ];
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       chat: this.chatItms,
-//       msg: "",
-//     };
-//   }
-
-//   scrollToBottom = () => {
-//     this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener("keydown", (e) => {
-//       if (e.keyCode == 13) {
-//         if (this.state.msg != "") {
-//           this.chatItms.push({
-//             key: 1,
-//             type: "",
-//             msg: this.state.msg,
-//             image:
-//               "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
-//           });
-//           this.setState({ chat: [...this.chatItms] });
-//           this.scrollToBottom();
-//           this.setState({ msg: "" });
-//         }
-//       }
-//     });
-//     this.scrollToBottom();
-//   }
-//   onStateChange = (e) => {
-//     this.setState({ msg: e.target.value });
-//   };
-
-//   render() {
-//     return (
-//       <div className="main__chatcontent">
-//         <div className="content__header">
-//           <div className="blocks">
-//             <div className="current-chatting-user">
-//               <Avatar
-//                 isOnline="active"
-//                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU"
-//               />
-//               <p>Tim Hover</p>
-//             </div>
-//           </div>
-
-//           <div className="blocks">
-//             <div className="settings">
-//               <button className="btn-nobg">
-//                 <i className="fa fa-cog"></i>
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="content__body">
-//           <div className="chat__items">
-//             {this.state.chat.map((itm, index) => {
-//               return (
-//                 <ChatItem
-//                   animationDelay={index + 2}
-//                   key={itm.key}
-//                   user={itm.type ? itm.type : "me"}
-//                   msg={itm.msg}
-//                   image={itm.image}
-//                 />
-//               );
-//             })}
-//             <div ref={this.messagesEndRef} />
-//           </div>
-//         </div>
-//         <div className="content__footer">
-//           <div className="sendNewMessage">
-//             <button className="addFiles">
-//               <i className="fa fa-plus"></i>
-//             </button>
-//             <input
-//               type="text"
-//               placeholder="Type a message here"
-//               onChange={this.onStateChange}
-//               value={this.state.msg}
-//             />
-//             <button className="btnSendMsg" id="sendMsgBtn">
-//               <i className="fa fa-paper-plane"></i>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-
-import React, { createRef, useEffect, useState } from 'react'
+import { ChatState } from '../../Context/ChatContext';
 
 const ChatContent = () => {
   const msgRef = createRef(null)
-  const [chats, setChats] = useState([
+  const [userChats, setUserChats] = useState([
     {
       key: 1,
       image:
@@ -210,8 +59,16 @@ const ChatContent = () => {
   ])
   const [msg, setMsg] = useState('')
 
+  // extracting states from context api
+  const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
+
   function scrollToBottom(){
     msgRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  // add new message
+  const addNewMessage = ()=>{
+    // add message
   }
   
   const onStateChange = (e)=>{
@@ -219,10 +76,11 @@ const ChatContent = () => {
   }
 
   useEffect(()=>{
-    scrollToBottom();
-  }, [chats])
+    // scrollToBottom();
+  }, [userChats])
 
-  return (
+  return ( 
+    selectedChat ?   
     <div className="main__chatcontent">
       <div className="content__header">
         <div className="blocks">
@@ -245,7 +103,7 @@ const ChatContent = () => {
       </div>
       <div className="content__body">
         <div className="chat__items">
-          {chats.map((itm, index) => {
+          {userChats.map((itm, index) => {
             return (
               <ChatItem
                 animationDelay={index + 2}
@@ -275,6 +133,10 @@ const ChatContent = () => {
           </button>
         </div>
       </div>
+    </div>
+    :  
+    <div className="main__chatcontent"> 
+      <h2 style={{margin: 'auto', color: "#b6b0ac" }}> Click to Select any chat </h2> 
     </div>
   )
 }
